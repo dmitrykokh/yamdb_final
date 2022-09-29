eeb0247c365c18630a2594967a96c882626116f9
Дмитрий Кох <dnkdnk7@yandex.ru>
2022-09-29 19:15:05 +0300

    day2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['51.250.19.82', 'web']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
