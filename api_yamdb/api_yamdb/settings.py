aa2e21da2b3fa77d25c2fe54431f749b8106bbac
Дмитрий Кох <dnkdnk7@yandex.ru>
2022-10-02 16:03:25 +0300

    after review

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -91,8 +91,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static/')),
-                    ('redoc', '/app/templates/')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
